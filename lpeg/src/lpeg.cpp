82ed723fc20d5b0a153736671000ec3bff92501f
Roman Silin <astrochili@users.noreply.github.com>
2022-07-25 21:28:55 +0300

    Fixed cpp methods names
    https://github.com/astrochili/defold-lpeg/issues/1

--- lpeg/src/lpeg.cpp
+++ lpeg/src/lpeg.cpp
@@ -9,7 +9,7 @@
 #include <dmsdk/sdk.h>
 #include "wrapper.h"
 
-dmExtension::Result InitializeMyExtension(dmExtension::Params* params)
+dmExtension::Result InitializeLPeg(dmExtension::Params* params)
 {
     luaopen_lpeg(params->m_L);
     lua_pop(params->m_L, 2);
@@ -18,10 +18,10 @@ dmExtension::Result InitializeMyExtension(dmExtension::Params* params)
     return dmExtension::RESULT_OK;
 }
 
-dmExtension::Result FinalizeMyExtension(dmExtension::Params* params)
+dmExtension::Result FinalizeLPeg(dmExtension::Params* params)
 {
     dmLogInfo("Finalize %s Extension\n", MODULE_NAME);
     return dmExtension::RESULT_OK;
 }
 
-DM_DECLARE_EXTENSION(lpeg, LIB_NAME, 0, 0, InitializeMyExtension, 0, 0, FinalizeMyExtension)
+DM_DECLARE_EXTENSION(lpeg, LIB_NAME, 0, 0, InitializeLPeg, 0, 0, FinalizeLPeg)
