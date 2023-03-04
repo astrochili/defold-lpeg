c4b2cdbbcae3f2b061b2865d4f1031d8022f4f42
Roman Silin <astrochili@users.noreply.github.com>
2023-03-04 14:17:23 +0200

    Fixed re.lua module

--- lpeg/re.lua
+++ lpeg/re.lua
@@ -3,7 +3,8 @@
 -- imported functions and modules
 local tonumber, type, print, error = tonumber, type, print, error
 local setmetatable = setmetatable
-local m = require"lpeg"
+local m = lpeg
+
 
 -- 'm' will be used to parse expressions, and 'mm' will be used to
 -- create expressions; that is, 're' runs on 'm', creating patterns
